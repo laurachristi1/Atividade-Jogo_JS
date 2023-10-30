7c39e4289837d196618d10a234014c263c8363fb
Halex-64 <halexgoncalves6@gmail.com>
2023-10-30 08:21:13 -0300

    Adicionando sons

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 
 //Efeitos Sonoros do Jogo
 const backgroundAudio = new Audio('../msc/backgroundAudio.mp3');
-const shootAudio = new Audio ('../msc/Laser Sound.mp3');
+const shootAudio = new Audio ('../msc/shot.mp3');
 
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
